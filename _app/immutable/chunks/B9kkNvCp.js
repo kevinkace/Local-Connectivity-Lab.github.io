0306d83a5c2d9d27641da79a24b84c636262a589
kevinkace <kevinkace@users.noreply.github.com>
2025-07-04 23:59:34 +0000

    deploy: d18b27f1a5954d63ed0c7076e111f863f9e088f3

---
+++ _app/immutable/chunks/B9kkNvCp.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_fqvnme)==null?void 0:s.base)??"/Local-Connectivity-Lab.github.io";var e;const a=((e=globalThis.__sveltekit_fqvnme)==null?void 0:e.assets)??t;export{a,t as b};
